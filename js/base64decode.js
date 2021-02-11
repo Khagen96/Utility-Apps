77ec791a7189b2bed202c16804ef73bf79233157
Khagen Borah <khagen.1996.borah@gmail.com>
2021-02-11 18:02:11 +0530

    decoder button fix

--- js/base64decode.js
+++ js/base64decode.js
@@ -1,16 +1,17 @@
 const urlInput = document.querySelector('#urlInput');
-const encodeButton = document.querySelector('#encode-button');
+const decodeButton = document.querySelector('#decode-button');
 const outputBox = document.querySelector('#urlOutput');
 const clearButton = document.querySelector('#clear-button');
 
-encodeButton.addEventListener('click', () => {
+decodeButton.addEventListener('click', () => {
     if (urlInput.value.length === 0) {
         // eslint-disable-next-line no-alert
-        alert('please enter text to encode');
+        alert('please enter text to decode');
     }
-    const base64 = btoa(urlInput.value);
-    outputBox.value = base64;
+    const base64Decode = atob(urlInput.value);
+    outputBox.value = base64Decode;
 });
+
 clearButton.addEventListener('click', () => {
     urlInput.value = '';
     outputBox.value = '';
