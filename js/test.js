b66772465f6c1ddaacaea7e6fa6a45e81c753ee6
Khagen Borah <khagen.1996.borah@gmail.com>
2021-02-21 21:16:30 +0530

    language translation complete

--- js/test.js
+++ js/test.js
@@ -4,26 +4,18 @@ const outputText = document.querySelector('#languageOutput');
 const inputOption = document.querySelector('#inputType');
 const outputOption = document.querySelector('#outputType');
 const language = {
-    Arabic: 'ar',
+
     English: 'en',
+    Hindi: 'hi',
     French: 'fr',
     Greek: 'el',
-    Gujarati: 'gu',
-    Hindi: 'hi',
     Indonesian: 'id',
     Italian: 'it',
     Japanese: 'ja-JP',
-    Kannada: 'kn',
     Korean: 'ko',
     Latin: 'la',
-    Malayalam: 'ml',
-    Marathi: 'mr',
     Mongolian: 'mn',
-    Nepali: 'ne',
     Spanish: 'es',
-    Tamil: 'ta',
-    Telugu: 'te',
-    Urdu: 'ur',
 };
 
 // console.log(inputOption.value);
@@ -31,55 +23,49 @@ const language = {
 const url = new URL('https://api.mymemory.translated.net/get');
 const inputPair = {
     q:inputText.value,
-    langPair:`${language[inputOption.value]}|${language[outputOption.value]}`
-}
+    langpair:`${language[inputOption.value]}|${language[outputOption.value]}`,
+};
+console.log(inputPair.langPair)
 Object.keys(inputPair).forEach((key) => {
     url.searchParams.append(key, inputPair[key]);
 });
 
 url.search = new URLSearchParams(inputPair).toString()
 console.log(url);
+const promise = fetch(url);
 
+promise
+.then((response)=>response.json())
+.then((json)=>{
+    // console.log(json.responseData.translatedText);
+    outputText.value = json.responseData.translatedText;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+})
 }
 
 
 
 const translateButton = document.querySelector('#translate-button');
 translateButton.addEventListener('click', () => {
-    // if (inputText.value.length === 0 || outputText.value.length === 0) {
-    //     alert('please enter text to encode/decode');
-    // }
-    translateText();
 
+        translateText();
+    
 });
 
 
 
 
-// const clearButton = document.querySelector('#clear-button');
-// const copyButton = document.querySelector('#copy-button');
-// clearButton.addEventListener('click', () => {
-//     urlInput.value = '';
-//     outputBox.value = '';
-// });
-// copyButton.addEventListener('click', () => {
-//     var copyText = document.querySelector('#urlOutput');
-//     copyText.select();
-//     document.execCommand("copy");
-//     alert("Copied the text: " + copyText.value);
-// });
+const clearButton = document.querySelector('#clear-button');
+const copyButton = document.querySelector('#copy-button');
+clearButton.addEventListener('click', () => {
+    const inputText = document.querySelector('#languageInput');
+    const outputText = document.querySelector('#languageOutput');
+    inputText.value = '';
+    outputText.value = '';
+});
+copyButton.addEventListener('click', () => {
+    var copyText = document.querySelector('#languageOutput');
+    copyText.select();
+    document.execCommand("copy");
+    alert("Copied the text: " + copyText.value);
+});
---
+++ js/translate.js
@@ -0,0 +1,71 @@
+function translateText() {
+    const inputText = document.querySelector('#languageInput');
+    const outputText = document.querySelector('#languageOutput');
+    const inputOption = document.querySelector('#inputType');
+    const outputOption = document.querySelector('#outputType');
+    const language = {
+    
+        English: 'en',
+        Hindi: 'hi',
+        French: 'fr',
+        Greek: 'el',
+        Indonesian: 'id',
+        Italian: 'it',
+        Japanese: 'ja-JP',
+        Korean: 'ko',
+        Latin: 'la',
+        Mongolian: 'mn',
+        Spanish: 'es',
+    };
+    
+    // console.log(inputOption.value);
+    // console.log(outputOption.value);
+    const url = new URL('https://api.mymemory.translated.net/get');
+    const inputPair = {
+        q:inputText.value,
+        langpair:`${language[inputOption.value]}|${language[outputOption.value]}`,
+    };
+    console.log(inputPair.langPair)
+    Object.keys(inputPair).forEach((key) => {
+        url.searchParams.append(key, inputPair[key]);
+    });
+    
+    url.search = new URLSearchParams(inputPair).toString()
+    console.log(url);
+    const promise = fetch(url);
+    
+    promise
+    .then((response)=>response.json())
+    .then((json)=>{
+        // console.log(json.responseData.translatedText);
+        outputText.value = json.responseData.translatedText;
+    
+    })
+    }
+    
+    
+    
+    const translateButton = document.querySelector('#translate-button');
+    translateButton.addEventListener('click', () => {
+    
+            translateText();
+        
+    });
+    
+    
+    
+    
+    const clearButton = document.querySelector('#clear-button');
+    const copyButton = document.querySelector('#copy-button');
+    clearButton.addEventListener('click', () => {
+        const inputText = document.querySelector('#languageInput');
+        const outputText = document.querySelector('#languageOutput');
+        inputText.value = '';
+        outputText.value = '';
+    });
+    copyButton.addEventListener('click', () => {
+        var copyText = document.querySelector('#languageOutput');
+        copyText.select();
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+    });
